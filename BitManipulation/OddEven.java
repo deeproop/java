ec98c357b823e4c8a0f1f2c27129f1332a877a77
Deep Roop <deepsinghroop@gmail.com>
2023-01-22 23:45:49 +0530

    .

--- BitManipulation/OddEven.java
+++ BitManipulation/OddEven.java
@@ -12,7 +12,7 @@ public class OddEven {
     }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("enter the number ");
+        System.out.print("enter the number");
         int n = sc.nextInt();
         OddOrEven(n);
     }

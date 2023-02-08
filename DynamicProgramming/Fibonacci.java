c3e159532b013890ec1c1cb5a2fc7d1f8275cd3d
Deep Roop <deepsinghroop@gmail.com>
2023-02-08 20:00:43 +0530

    tabluation added

--- DynamicProgramming/Fibonacci.java
+++ DynamicProgramming/Fibonacci.java
@@ -27,10 +27,22 @@ public class Fibonacci {
         return f[n];
 
     }
+
+    public static int fibTabulation(int n) { //O(n)
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n = 5;
         int f[] = new int[n+1];
 
-        System.out.println(fibb(n, f));
+        System.out.println(fibTabulation(n));
     }
 }
